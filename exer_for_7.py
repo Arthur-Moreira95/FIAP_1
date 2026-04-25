7affea1f9ea03f37692f431326b7b01ba74a92a1
Arthur Moreira <arthurmoreirac401@gmail.com>
2026-04-24 23:15:56 -0300

    atualização do exercício 7 de "for"

--- exer_for_7.py
+++ exer_for_7.py
@@ -1,6 +1,11 @@
-n = int(input("Digite um numero inteiro qualquer: "))
-d = 1
-
-if n % d == 0:
-    print(f"{n} é um numero primo")
-
+n = int(input("Insira um numero para verificar se é primo ou não: "))
+i = 2
+x = 0
+for i in range(i, n):
+    if n % i == 0:
+        x+=1
+        break
+if x == 0:
+    print("O número é primo")
+else:
+    print("O número não é primo")
