951c4b0560f06f5f99be88242ecda571526995d3
Arthur Moreira <arthurmoreirac401@gmail.com>
2026-04-25 19:32:42 -0300

    Correção

--- exer_lista_2.py
+++ exer_lista_2.py
@@ -5,13 +5,9 @@ soma_media = 0
 
 for _ in range(10):
    numero = int(input("Digite um numero: "))
-
    soma_media += numero
-
    if numero % 2 == 0:
     soma_par += numero
-
-
 
 media = soma_media / 10
 lista_media.append(media)
